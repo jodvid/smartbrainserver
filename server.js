6f35e15e04790078e5521420e5c4fa85d2260fe0
jo david <jo.wbdev@gmail.com>
2021-12-22 13:46:09 +0800

    changing host

--- server.js
+++ server.js
@@ -15,11 +15,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-    //   port : 3306,
-      user : 'postgres',
-      password : 'test',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true,
     }
   });
 
